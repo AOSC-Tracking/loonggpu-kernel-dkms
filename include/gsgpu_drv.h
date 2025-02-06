6a96edffd9f5bbc1b2cf3a488032623b0b2a361c
Kexy Biscuit <kexybiscuit@aosc.io>
2025-02-06 18:12:15 +0800

    AOSCOS: loonggpu: remove driver date
    Following upstream changes.
    
    Fixes: cb2e1c2136f7 ("drm: remove driver date from struct drm_driver and all drivers")
    Fixes: "chore: import 1.0.1-alpha-lnd25.5"
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

--- include/gsgpu_drv.h
+++ include/gsgpu_drv.h
@@ -10,7 +10,9 @@
 
 #define DRIVER_NAME		"loonggpu"
 #define DRIVER_DESC		"Loongson LGxx GPU Driver"
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 14, 0))
 #define DRIVER_DATE		"20241219"
+#endif
 
 long gsgpu_drm_ioctl(struct file *filp,
 		      unsigned int cmd, unsigned long arg);
