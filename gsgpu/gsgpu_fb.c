09c4bae40f4d1191a3d8a8caab81a70f8c5d35c3
Xi Ruoyao <xry111@xry111.site>
2025-06-23 20:31:20 +0800

    loonggpu: Remove a redundant gsgpu_fbdev_client_hotplug call
    It's not needed anymore as now the generic drm_client_register() calls
    the hotplug hook.
    
    Link: https://git.kernel.org/torvalds/c/27655b9bb9f0
    Signed-off-by: Xi Ruoyao <xry111@xry111.site>

--- gsgpu/gsgpu_fb.c
+++ gsgpu/gsgpu_fb.c
@@ -365,10 +365,6 @@ void gsgpu_fbdev_setup(struct gsgpu_device *adev)
 		goto err_drm_client_init;
 	}
 
-	ret = gsgpu_fbdev_client_hotplug(&fb_helper->client);
-	if (ret)
-		drm_dbg_kms(adev->ddev, "client hotplug ret = %d\n", ret);
-
 	drm_client_register(&fb_helper->client);
 
 	return;
